7f6b8834f636340f4977bfaec5dd8bafbba894e1
casacava <cassiopeiacavazos@gmail.com>
2022-06-09 22:38:58 -0500

    adding scores + tally

--- script.js
+++ script.js
@@ -41,5 +41,89 @@ else if (computerScore === 2 || computerScore === 4) {
    } 
 }
 }
-
-
+function displayResults(str) {
+   container.animate([{ opacity: 0 }, { opacity: 1 }], {
+     duration: 300,
+     fill: "forwards",
+     iterations: 1,
+     delay: 0,
+     easing: "ease-out",
+   });
+   container.textContent = str;
+ }
+ 
+ function declareWinner() {
+   rplContent();
+   if (playerScore > computerScore) {
+     endDesc.textContent = "You win! Congrats champ!!";
+     returnMainBtn.innerText = "Play Again";
+   } else {
+     endDesc.textContent = "You lost......";
+     returnMainBtn.innerText = "Try Again?";
+   }
+   fadeIn();
+ 
+   let endDescSpan = endDesc.querySelectorAll("span");
+   endDescSpan = Array.from(endDescSpan);
+ 
+   endDescSpan[endDescSpan.length - 1].ontransitionend = () => {
+     returnMainBtn.classList.add("fade-in");
+     /*returnMainBtn.animate([{ opacity: 0 }, { opacity: 1 }], {
+       duration: 00,
+       fill: "forwards",
+       iterations: 1,
+       delay: 0,
+       easing: "ease-in",
+     });*/
+   };
+ }
+ 
+ function rplContent() {
+   main.classList.add("disappear");
+   endAlrt.classList.remove("disappear");
+   desc.classList.remove("animate");
+   endDesc.classList.add("animate");
+ 
+   returnMainBtn.addEventListener("click", () => {
+     main.classList.remove("disappear");
+     endAlrt.classList.add("disappear");
+     desc.classList.add("animate");
+     returnMainBtn.classList.remove("fade-in");
+     resetGame();
+   });
+ }
+function resetGame() {
+   fadeIn();
+   container.textContent = "";
+   playerScore = 0;
+   computerScore = 0;
+   keepPlayerScore();
+   keepCompScore();
+ }
+ 
+function keepPlayerScore() {
+   let playerScoreBox = document.querySelector("#player-score");
+ 
+   playerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+     duration: 300,
+     fill: "forwards",
+     iterations: 1,
+     delay: 0,
+     easing: "ease-out",
+   });
+ 
+   playerScoreBox.textContent = playerScore;
+ }
+ function keepCompScore() {
+   let computerScoreBox = document.querySelector("#computer-score");
+ 
+   computerScoreBox.animate([{ opacity: 0 }, { opacity: 1 }], {
+     duration: 300,
+     fill: "forwards",
+     iterations: 1,
+     delay: 0,
+     easing: "ease-out",
+   });
+ 
+   computerScoreBox.textContent = computerScore;
+ }
